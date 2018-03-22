5ea63fced1b55d247363f425344a004e530dd590
ValikVasilkovskiy <valik.vasilkovskiy@gmail.com>
2018-03-22 16:36:28 +0200

    add key for min_number

---
+++ Friends.py
@@ -0,0 +1,28 @@
+class Friends():
+    def __init__(self, connections):
+        self.connections = connections
+
+    def add(self, data):
+        if data in self.connections:
+            return False
+        else:
+            self.connections.append(data)
+            return True
+
+    def remove(self, data):
+        if data in self.connections:
+            self.connections.remove(data)
+            return True
+        else:
+            return False
+
+# test add (self.connection append data + Tru when set or reversed set not in self.connections )
+f = Friends([{'1', '2'}, {'3', '1'}])
+print(f.add({'2', '1'}))
+print(f.add({'5', '1'}), end='\n\n')
+
+# test (self.connections.remove(i) when data in self.connections)
+
+s = Friends([{'1', '2'}, {'3', '1'}])
+print(s.remove({'1', '3'}))
+print(s.remove({'4', '5'}))
--- Test.py
+++ Test.py
@@ -1,3 +1,10 @@
+s = [{'1', '2'}, {'3', '4'}]
+data = {'2', '1'}
+
+for i in s:
+    if i == data:
+        print(True)
+
 
 
 
