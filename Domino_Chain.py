ccbecb815409a4d673fa0d10010ff222c68de70d
ValikVasilkovskiy <valik.vasilkovskiy@gmail.com>
2018-03-30 18:51:32 +0300

    TODO optimize checkio task domino_chain

--- Domino_Chain.py
+++ Domino_Chain.py
@@ -1,12 +1,13 @@
 import itertools
 
 def domino_chain(tiles: str):
-    stack, temp_stack, chain_stack, r = [], [], [], len(tiles.split(','))
+    stack, temp_stack, chain_stack, r, next_stack = [], [], [], len(tiles.split(',')), []
     for i in tiles:
         if i.isdigit():
             temp_stack.append(int(i))
             if len(temp_stack) == 2:
                 stack.append(temp_stack)
+                next_stack.append(temp_stack)
                 stack.append(list(reversed(temp_stack)))
                 temp_stack = []
     for i in list(itertools.permutations(stack, r)):
@@ -14,7 +15,7 @@ def domino_chain(tiles: str):
             if i[j][1] != i[j+1][0]:
                 break
         else:
-            for part in stack:
+            for part in next_stack:
                 if part in i or list(reversed(part)) in i:
                     continue
                 else:
@@ -25,9 +26,9 @@ def domino_chain(tiles: str):
     return len(chain_stack)
 
 # test
-print(domino_chain("0-2, 0-5, 1-5, 1-3, 5-5")) # return 1 combination
-print(domino_chain("1-5, 2-5, 3-5, 4-5, 3-4")) # return 2 combination
-print(domino_chain("0-5, 1-5, 2-5, 3-5, 4-5, 3-4")) # return 0 combination
+#print(domino_chain("0-2, 0-5, 1-5, 1-3, 5-5")) # return 1 combination
+#print(domino_chain("1-5, 2-5, 3-5, 4-5, 3-4")) # return 2 combination
+#print(domino_chain("0-5, 1-5, 2-5, 3-5, 4-5, 3-4")) # return 0 combination
 print(domino_chain("1-2, 1-4, 1-5, 1-6, 1-1, 2-5, 4-6")) # 28
 
 
