6c12d698d2eb69bf2d77d38d505a61f28309a218
ValikVasilkovskiy <valik.vasilkovskiy@gmail.com>
2018-04-11 12:32:07 +0300

    update unix match

--- Test2.py
+++ Test2.py
@@ -1,29 +1,27 @@
 import itertools
 
 def domino_chain(tiles: str):
-    list_stack = []
-    temp_stack = []
+    stack, temp_stack = [], ''
     for i in tiles:
         if i.isdigit():
-            temp_stack.append(int(i))
+            temp_stack += i
             if len(temp_stack) == 2:
-                list_stack.append(temp_stack)
-                if list(reversed(temp_stack)) not in list_stack:
-                    list_stack.append(list(reversed(temp_stack)))
-                temp_stack = []
-    tuple_stack = tuple(map(lambda x: tuple(x), list_stack))
-    comb1 = []
-    combinations = set(itertools.permutations(tuple_stack, 2))
-    for i in combinations:
+                stack.append(temp_stack)
+                temp_stack = ''
+    combinations = []
+
+    for i in tuple(itertools.permutations(stack, len(stack))):
         for j in range(len(i)-1):
-            if i[j][1] == i[j+1][0]:
-                if set(i[j]) != set(i[j+1]):
-                    comb1.append(i)
-    set(comb1)
-    for i in tuple(itertools.permutations(comb1, 2)):
-        print(i)
+            if i[j][0] not in i[j+1] and i[j][1] not in i[j+1]:
+                break
+        else:
+            if i not in combinations and tuple(reversed(i)) not in combinations:
+                combinations.append(i)
+    return len(combinations)
 
 
-
-print(domino_chain("0-2, 0-5, 1-5, 1-3, 5-5"))
+print(domino_chain("0-2, 0-5, 1-5, 1-3, 5-5")) # return 1 combination
+print(domino_chain("1-5, 2-5, 3-5, 4-5, 3-4")) # return 2 combination
+print(domino_chain("0-5, 1-5, 2-5, 3-5, 4-5, 3-4")) # return 0 combination
+print(domino_chain("0-1, 0-2, 1-3, 1-2, 3-4, 2-4")) # 6
 
